5d8ffa8e95dd54a22735d0d54c9751fad570c243
Lisbeth <liisenca8@gmail.com>
2023-09-26 00:11:12 -0500

    este es mi primer commit

---
+++ clase2.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+mi_variable = 23
+nombre = "Lissy "
+nombre + " "+ str(mi_variable) 
+print("mi nombre es",nombre,"y mi edad es", mi_variable )
+cosas =["platos"," casas" , "correa","cinta"]
+f"mi nombre es {nombre}"
+
---
+++ python.py
@@ -0,0 +1,22 @@
+import os 
+os.listdir(".")
+import pandas as pd
+pd.DataFrame({"a":[1]})
+import glob 
+glob.glob("*")
+from glob import glob
+glob("*")
+import matplotlib.pyplot as plt 
+"lissy"
+type("lissy")
+dict={"a":["lissy"],"b":[36],"c":[True]}
+dict["c"]
+mi_lista=[1,2,3]
+mi_lista[2]
+import pandas as pd 
+df= pd.DataFrame(dict)
+df
+for i in dict.values (): 
+  print (i)
+  
+  
